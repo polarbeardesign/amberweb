69d0e2e7fe227437e9c1c354b2ce050585d82e71
Jim Ivanoff <jim@polarbeardesign.net>
2015-09-15 20:34:30 -0500

    adding in images and sliders

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,18 @@
-  <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About Amber Lodging</a></li>
-    <li><a href="#">How We Work</a></li>
-    <li><a href="#">Current Resident Request</a></li>
-    <li><a href="#">Request Information</a></li>
-    <li><a href="#">Contact Us</a></li>
-  </ul>
+	<div class="menu-link">
+		<div class="show_menu">
+			<div class="bar-icon-wrapper">
+				<span class="bar-icon"></span>
+				<span class="bar-icon"></span>
+				<span class="bar-icon"></span>
+			</div>
+		</div>
+
+		<ul class="mobile main-navigation">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="index.php">About Amber Lodging</a></li>
+			<li><a href="index.php">How We Work</a></li>
+			<li><a href="index.php">Current Resident Request</a></li>
+			<li><a href="index.php">Request Information</a></li>
+			<li><a href="index.php">Contact Us</a></li>
+		</ul>
+  </div>
--- index.php
+++ index.php
@@ -5,8 +5,33 @@
   
   <link rel="stylesheet" href="inc/style.css" type="text/css" media="screen">
   <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-  
+  <script  type="text/javascript" src="inc/jquery-1.11.3.min.js"></script>
+  <script type="text/javascript" src="inc/jquery.tongue.min.js"></script>
   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
+	<script type="text/javascript" language="javascript">
+
+	$(document).ready(function() {
+		$(".show_menu").click(function () {
+			$(this).siblings($(this)).toggle();
+		});
+	});
+
+	$(document).ready(function() {
+		$(".show_detail").click(function () {
+			$(this).siblings($(this)).toggle( "slow" );
+		});
+	});
+
+	$('#tongue').tongue({
+		'position'      : 'bottom',
+		'tongue_content': '.tongue-content',
+		'start_speed'   : 500,
+		'end_speed'     : 400
+	});
+
+	</script>
+
 </head>
 
 <body>
@@ -23,12 +48,32 @@
 
 <section class="main">
   <article>
-    <img src='img/slider_antarctica.png' width="100%"/>
+    <div id="tongue"><img src='img/slider_antarctica.png' width="100%"/>
+			<div class="tongue-content">
+				<strong>Let's say you're trying to sign a lease in some country on the other side of the world.</strong>
+				But by the time you sign the lease in the US office it would already be lost to someone 
+				competing for that location. But we have a proprietary, fluid system that allows us to
+				vet, sign and fund the lease for our clients, all over the world.
+			</div>
+		</div>
+		<article>
     <div class="row">
+			<div class="content-cell-wide">
+				<img src="img/hww_1625_1083.png" width="100%" alt="How we work" />
+			</div>
+			<div class="content-cell">
+				<img src="img/ri_1011_1083.png" width="100%" alt="Request information" />
+			</div>
     </div>
     <div class="row">
+			<div class="content-cell">
+				<img src="img/aa_1020_1085.png" width="100%" alt="About Amber" />
+			</div>
+			<div class="content-cell-wide">
+				<img src="img/cr_1614_1082.png" width="100%" alt="Current resident request" />
+			</div>
     </div>
-  </article>
+
 </section>
 
 <footer>
