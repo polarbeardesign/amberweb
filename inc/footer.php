7d6925c80901c1bb2d55fb990cd6efd6d49bdc88
Jim Ivanoff <jim@polarbeardesign.net>
2015-09-20 16:45:59 -0500

    update to set footer log width

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
   <div class="copyright">
     <img 
     srcset="img/logo_bw.svg 208w"
-    src='img/logo_bw.png' width="100%" 
+    src='img/logo_bw.png' width="200px" 
     alt="Amber Lodging" />
   </div>
