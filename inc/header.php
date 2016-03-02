7e2bb7ad52e908c523e52a8e72c0c0ef2fb3f73f
Jim Ivanoff <jim@polarbeardesign.net>
2016-03-02 15:42:58 -0600

    build out of property details page, added reserve now form

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,9 @@
-<div id="black_overlay" style="width: 100%;"> </div>
-	<form action="client_home.php" method="post">
-	<div class="login_form">
-	<img src="img/form_close.png" width="20" height="20" class="close" />
+<div id="black_overlay" style="width: 100%;">
+</div>
+
+  <form action="client_home.php" method="post">
+    <div class="login_form">
+    <p class="close">&times;</p>
     <h3>Log In</h3>
     <label for="vemail">Email</label>
     <input type="text" id="vemail" name="email" size="19.2" placeholder="Your Email">
@@ -10,15 +12,15 @@
     <label><input type="checkbox" /> Remember me</label>
     <input type="submit" value="Log In" id="submit" class="li-button">
     <a href="#" class="forgot-pwd">Forgot Password?</a>
-
-</div>
+    </div>
   </form>
-  <div class="logo">
+
+<div class="logo">
   <a href="index.php"><img 
-    srcset="img/logo.svg 208w"
-    src='img/logo.png' width="100%" 
-    alt="Amber Lodging" /></a>
-  </div>
-  <div class="tagline"><span class="heavy-italic">
-  Feel right at home</span>, anywhere on earth.
-  </div>
+  srcset="img/logo.svg 208w"
+  src='img/logo.png' width="100%" 
+  alt="Amber Lodging" /></a>
+</div>
+<div class="tagline">
+  <span class="heavy-italic">Feel right at home</span>, anywhere on earth.
+</div>
--- property_detail.php
+++ property_detail.php
@@ -2,25 +2,61 @@
 
 $client = "Johnny Washington";
 
-$property_name = "Gables West Avenue";
-$location_id = "987654";
-$property_address = "300 West Avenue";
-$property_city = "Austin";
-$property_state = "TX";
-$property_postal_code = "78701";
-$property_country_code = "US";
-$move_in = "Jan 20";
-$move_out = "Feb19";
-$daily_price = "260";
-$min_stay = "30";
+$search_parameters = array('city' => "Austin", 'state' => "TX", 'move_in' => "Mar 01", 'move_out' => "Mar 31");
 
-$image_0 = "property_feature_1.jpg";
+$selected_property = array('property_name' => "Gables West Avenue", 
+                           'neighborhood' => "Downtown", 
+                           'id' => "987654", 
+                           'size' => "1BD / 1BA", 
+                           'property_address' => "300 West Avenue", 
+                           'property_city' => "Austin", 
+                           'property_state' => "TX", 
+                           'property_postal_code' => "78701", 
+                           'property_country_code' => "US",
+                           'property_latitude' => "30.2680695",
+                           'property_longitude' => "-97.7538387",
+                           'daily_price' => "260", 
+                           'min_stay' => "30",
+                           'images' => array("property_feature_1.jpg", "property_feature_2.jpg", "property_feature_3.jpg", "property_feature_4.jpg", "property_feature_5.jpg"), 
+                           'features' => array("One bedroom, one full bathroom","785 sqft","2nd floor","Balcony with sitting area","High Speed Internet","Washer/dryer in unit","Fully furnished","Full size kitchen"),
+                           'amenities' => array("Built in 2001, recently renovated","Ceiling Fans","Club Room","Extra Storage Available","High Speed Internet","Laundry On Site","Pool","Public Transportation Near By","Rooftop Amenities","Wireless High Speed Internet"));
 
-$property_features = array("One bedroom, one full bathroom","785 sqft","2nd floor","Balcony with sitting area","High Speed Internet","Washer/dryer in unit","Fully furnished","Full size kitchen");
-$property_amenities = array("Built in 2001, recently renovated","Ceiling Fans","Club Room","Extra Storage Available","High Speed Internet","Laundry On Site","Pool","Public Transportation Near By","Rooftop Amenities","Wireless High Speed Internet");
-
-$nearby_properties = array(0 => array('property_thumb' => "property_987654_thumb.jpg", 'property_name' => "Green Pastures", 'neighborhood' => "Downtown", 'id' => "987654", 'size' => "1BD / 1BA", 'property_address' => "800 Brazos Street", 'property_city' => "Austin", 'property_state' => "TX", 'property_postal_code' => "78701", 'property_country_code' => "US"),
-                           1 => array('property_thumb' => "property_987654_thumb.jpg", 'property_name' => "Travis Heights Arms", 'neighborhood' => "Downtown", 'id' => "123456", 'size' => "2BD / 1BA", 'property_address' => "1136 Travis Heights Boulevard", 'property_city' => "Austin", 'property_state' => "TX", 'property_postal_code' => "78704", 'property_country_code' => "US"));
+$nearby_properties = array(0 => array('property_thumb' => "property_987654_thumb.jpg", 
+                                      'property_name' => "Green Pastures", 
+                                      'neighborhood' => "Downtown", 
+                                      'id' => "987654", 
+                                      'size' => "1BD / 1BA", 
+                                      'property_address' => "800 Brazos Street", 
+                                      'property_city' => "Austin", 
+                                      'property_state' => "TX", 
+                                      'property_postal_code' => "78701", 
+                                      'property_country_code' => "US",
+                                      'daily_price' => "198",
+                                      'min_stay' => "30"),
+                           1 => array('property_thumb' => "property_987654_thumb.jpg", 
+                                      'property_name' => "Travis Heights Arms", 
+                                      'neighborhood' => "Downtown", 
+                                      'id' => "123456", 
+                                      'size' => "2BD / 1BA", 
+                                      'property_address' => "1136 Travis Heights Boulevard", 
+                                      'property_city' => "Austin", 
+                                      'property_state' => "TX", 
+                                      'property_postal_code' => "78704", 
+                                      'property_country_code' => "US",
+                                      'daily_price' => "198",
+                                      'min_stay' => "30"),
+                           2 => array('property_thumb' => "property_987654_thumb.jpg", 
+                                      'property_name' => "Lakeside Apartments", 
+                                      'neighborhood' => "Downtown", 
+                                      'id' => "234567", 
+                                      'size' => "2BD / 1BA", 
+                                      'property_address' => "85 Trinity Street", 
+                                      'property_city' => "Austin", 
+                                      'property_state' => "TX", 
+                                      'property_postal_code' => "78701", 
+                                      'property_country_code' => "US",
+                                      'daily_price' => "295",
+                                      'min_stay' => "30"));
 
 ?>
 
@@ -33,8 +69,12 @@ $nearby_properties = array(0 => array('property_thumb' => "property_987654_thumb
 
   <link rel="stylesheet" href="inc/bootstrap.min.css" type="text/css" media="screen">
   <link rel="stylesheet" href="inc/style.css" type="text/css" media="screen">
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,400italic,800italic,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:100,400,600,400italic,600italic,800italic,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
 
 </head>
@@ -44,40 +84,145 @@ $nearby_properties = array(0 => array('property_thumb' => "property_987654_thumb
 <div class="title-bar alc">Amber Lodging Company</div>
 <div class="title-bar client-name"><?php echo $client ?> <span class="caret"></span></div>
 
+<div id="black_overlay" style="width: 100%;">
+</div>
+
+
+  <div class="reserve_now_form">
+  <form action="" method="post">
+  <p class="close">&times;</p>
+  <h1>Guest Details For Reservation</h1>
+  <p class="small-print"><em>All reservations are subject to availability. Changes to the reservation may alter pricing and availability.</em></p>
+  <p class="small-print"><em>Reservations are not set until confirmed. Confirmations are sent Mon-Fri between the hours of 7-7CST</em></p>
+<section class="container-fluid no-padding">
+  <div class="row">
+      <div class="col-md-6">
+        <label for="first_name">First Name</label>
+        <input type="text" id="first_name" name="first_name" />
+      </div>
+      <div class="col-md-6">
+        <label for="last_name">Last Name</label>
+        <input type="text" id="last_name" name="last_name" />
+      </div>
+  </div>
+  <div class="row">
+      <div class="col-md-6">
+        <label for="reference_no">File # / Reference # / Employee ID</label>
+        <input type="text" id="reference_no" name="reference_no" />
+      </div>
+      <div class="col-md-6">
+        <label for="department">Department</label>
+        <input type="text" id="department" name="department" />
+      </div>
+  </div>
+  <div class="row">
+      <div class="col-md-6">
+        <label for="client_id">Employer / Client</label>
+        <input type="text" id="client_id" name="client_id" />
+      </div>
+      <div class="col-md-6">
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email">
+      </div>
+  </div>
+  <label for="email">Important Notes</label>
+  <textarea id="notes" rows="7" class="boxsizingBorder" ></textarea>
+
+
+  <hr class="form-hr"/>
+  <h2>Agent requesting information</h2>
+  <div class="row">
+      <div class="col-md-6">
+        <label for="guest_first_name">First Name</label>
+        <input type="text" id="guest_first_name" name="guest_first_name">
+      </div>
+      <div class="col-md-6">
+        <label for="guest_last_name">Last Name</label>
+        <input type="text" id="guest_last_name" name="guest_last_name">
+      </div>
+  </div>
+  <div class="row">
+      <div class="col-md-6">
+        <label for="guest_email">Email</label>
+        <input type="text" id="guest_email" name="guest_email">
+      </div>
+      <div class="col-md-6">
+        <label for="guest_phone">Phone Number</label>
+        <input type="text" id="guest_phone" name="guest_phone">
+      </div>
+
+  </div>
+  <div class="text-center" style="margin: 0 auto;">
+    <input type="submit" value="Confirm Reservation" id="submit" class="li-button">
+  </div>
+</form>
+</div>
 
 <div id="container">
 
 <div class="info-bar">
   <span class="info-back"><i class="fa fa-long-arrow-left"></i> Back to properties</span>
-  <span class="location"><i class="fa fa-map-marker"></i> <?php echo $property_city, ", ", $property_state ?></span>
-  <span class="dates"><i class="fa fa-calendar"></i> <?php echo $move_in, " - ", $move_out ?></span>
+  <span class="location"><i class="fa fa-map-marker"></i> <?php echo $search_parameters[city], ", ", $search_parameters[state] ?></span>
+  <span class="dates"><i class="fa fa-calendar"></i> <?php echo $search_parameters[move_in], " - ", $search_parameters[move_out] ?></span>
   <span class="update-search"><a href="#">edit search</a></span>
 </div>
 
 <section class="property-detail container-fluid">
   <div class="row">
     <div class="col-md-7">
-      <img src="img/clients/<?php echo $image_0 ?>" alt="View 1" />
+
+      <div id="feature_property-carousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+
+<?php
+           foreach($selected_property[images] as $key => $value) {  echo 
+              '<li data-target="#feature_property-carousel" data-slide-to="' . $key . '"';
+              if ($key == 0) { 
+                 echo ' class="active"';
+                 } 
+              echo '></li>';
+              }
+          ?>
+        </ol>
+ 
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <?php
+          foreach($selected_property[images] as $key => $value) {  echo 
+              '<div class="item ' ;
+              if ($key == 0) { 
+                 echo 'active';
+                 } 
+              echo '">
+                <img src="img/clients/' . $value . '" alt="...">
+              </div>';
+              }
+
+          ?>
+
+        </div>
+       </div>
+
     </div>
     <div class="col-md-4">
-      <h1 class="location-title"><?php echo $property_name ?></h1>
-      <div class="location-id">ID# <?php echo $location_id ?></div> 
-      <div class="location-address"><?php echo $property_address, " | ", $property_city, ", ", $property_state, " ", $property_postal_code, ", ", $property_country_code ?></div>
+      <h1 class="location-title"><?php echo $selected_property[property_name] ?></h1>
+      <div class="location-id">ID# <?php echo $selected_property[id] ?></div> 
+      <div class="location-address"><?php echo $selected_property[property_address], " | ", $selected_property[property_city], ", ", $selected_property[property_state], " ", $selected_property[property_postal_code], ", ", $selected_property[property_country_code] ?></div>
       <div class="row property-numbers">
         <div class="col-md-5">
-          <div class="daily-price">$<?php echo $daily_price ?></div> USD
+          <div class="daily-price">$<?php echo $selected_property[daily_price] ?></div> USD
           <div class="small-print">avg/night</div>
         </div>
         <div class="col-md-7">
-          <div class="min-stay"><?php echo $min_stay ?></div> DAY
+          <div class="min-stay"><?php echo $selected_property[min_stay] ?></div> DAY
           <div class="small-print">min stay</div>
         </div>
       </div>
       <div class="row">
       <div class="btn btn-primary reserve-now">Reserve This Property Now</div>
-        <p class="small text-center">or</p>
-        <p class="small text-center">
-          <a href="">Request Info</a>
+        <p class="small-print text-center">or</br />
+          <a href="#">Request Info</a>
         </p>
         <p class="small-print"><em>All reservations are subject to availability. Changes to the reservation may alter pricing and availability.</em></p>
         <p class="small-print"><em>Reservations are not set until confirmed. Confirmations are sent Mon-Fri between the hours of 7-7CST</em></p>
@@ -88,11 +233,11 @@ $nearby_properties = array(0 => array('property_thumb' => "property_987654_thumb
     <div class="col-md-7">
       <h2 class="property-about">About This Rental</h2>
         <ul class="property-features">
-          <?php foreach($property_features as $value) { echo '<li>'.$value.'</li>'; } ?>
+          <?php foreach($selected_property[features] as $value) { echo '<li>'.$value.'</li>'; } ?>
         </ul>
       <h2 class="property-amenities">Property Amenities</h2>
         <ul class="property-amenities">
-          <?php foreach($property_amenities as $value) { echo '<li>'.$value.'</li>'; } ?>
+          <?php foreach($selected_property[amenities] as $value) { echo '<li>'.$value.'</li>'; } ?>
         </ul>
     </div>
     <div class="col-md-4 cal-section">
@@ -223,17 +368,23 @@ $nearby_properties = array(0 => array('property_thumb' => "property_987654_thumb
  
  <?php 
  foreach($nearby_properties as $inner) { echo    
-      '<div class="nearby-box">
+      '<div class="nearby-box  container-fluid">
         <div class="row">
-          <div class="col-md-3">
+          <div class="col-sm-12 col-md-3">
             <img src="img/clients/'.$inner[property_thumb].'" alt="<##>" />
           </div>
-          <div class="col-md-4">
+          <div class="col-sm-12 col-md-6">
           <h1 class="nearby">'.$inner[property_name].'</h1>
           <p class="neighborhood">'.$inner[neighborhood].'</p>
           <p>ID# '.$inner[id] . ' | ' . $inner[size].'</p>
           <p class="nearby-address">'.$inner[property_address] . '<br />
           ' .$inner[property_city] . ', '.$inner[property_state] . ' ' .$inner[property_postal_code] . ', '.$inner[property_country_code] . ' </p>
+           <p class="min-stay">minimum '.$inner[min_stay].' day stay</p>
+          </div>
+          <div class="col-sm-12 col-md-3 text-right">
+          <div class="daily-price">$'.$inner[daily_price] . '</div> <a href="">USD</a>
+          <div class="small-print">avg/night</div>
+          <div class="btn btn-primary view-details">View Details</div>
           </div>
           </div>
       </div>
@@ -245,8 +396,9 @@ $nearby_properties = array(0 => array('property_thumb' => "property_987654_thumb
     </div>
     <div class="col-md-4">
     &nbsp;
-    <h3>Getting to the property</h3>
-    
+    <h3 class="map-header">Getting to the property</h3>
+    <img src="http://maps.googleapis.com/maps/api/staticmap?zoom=11&size=350x350&maptype=roadmap&markers=color:red%7Clabel:X%7C<?php echo "$selected_property[property_latitude],$selected_property[property_longitude]" ?>&sensor=false" %>
+
     
   </div>    
 </section>
@@ -256,6 +408,17 @@ $nearby_properties = array(0 => array('property_thumb' => "property_987654_thumb
 
 </body>
 
-
-
+  <script type="text/javascript" language="javascript">
+    $(document).ready(function() {
+        $(".reserve-now").click(function() {
+          var h = $("body").height() + 'px';
+          $("#black_overlay").css({"height":h,"visibility":"visible"});
+          $(".reserve_now_form").css('display','block');
+        });
+        $(".close, #submit").click(function() {
+        $(".reserve_now_form").css('display','none');
+        $("#black_overlay").css("visibility","hidden");
+      });
+    });
+  </script>
 </html>
