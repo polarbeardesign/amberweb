e123cd4f5ff79364f56a81f4654dba3cce5e5b57
Jim Ivanoff <jim@polarbeardesign.net>
2016-09-16 15:17:08 -0500

    updates to files required for datepicker

--- reservation_request.php
+++ reservation_request.php
@@ -38,34 +38,25 @@ $result_properties = array('property_thumb' => "Content/img/clients/property_fea
 <head>
     <title>Offer :: Amber Lodging</title>
 
+    <link rel="stylesheet" href="Content/bootstrap.min.css" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,800italic,700' rel='stylesheet' type='text/css' />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="Content/themes/base/all.css" type="text/css" />
+    <link rel="stylesheet" href="Content/style.css" type="text/css" media="screen" />
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+
+
+
 <!-- JQuery -->
   <script type="text/javascript" src="Scripts/jquery-2.2.3.min.js"></script>
-<!-- for property slider on property details page -->
-  <script type="text/javascript" src="Scripts/bootstrap.min.js"></script>
 <!-- for JQuery'd UI elements: tooltips, etc -->
   <script type="text/javascript" src="Scripts/jquery-ui-1.11.4.min.js"></script>
 <!-- for form vaidations -->  
   <script src="inc/languages/jquery.validationEngine-en.js" type="text/javascript" charset="utf-8"></script>
   <script src="inc/jquery.validationEngine.js" type="text/javascript" charset="utf-8"></script>
-
-<!-- for imported fonts and icons -->
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:100,400,500,600,400italic,600italic,800italic,700' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<!-- Bootstrap styles -->
-
-  <link rel="stylesheet" href="Content/themes/base/all.css" type="text/css">
-
 <!-- for form validation styling -->
   <link rel="stylesheet" href="inc/validationEngine.jquery.css" type="text/css"/>
 
-
-    <link rel="stylesheet" href="Content/bootstrap.min.css" type="text/css" />
-<!-- 
-    <link rel="stylesheet" href="Content/style.css" type="text/css" media="screen" />
--->
-    <link rel="stylesheet" href="https://gist.githubusercontent.com/polarbeardesign/7008e9087c90a5d6fb58e0bcb1cb567a/raw/c7edd30dafd215df70c7850c5e6e663bd79d89e5/amber_style.css" type="text/css" media="screen" />
-        
-  <link rel="stylesheet" href="Content/style.css" type="text/css" media="screen" />
     <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
 
 
